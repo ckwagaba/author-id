86d375d3b89712b66b82e26f1e7457ab7d2678c8
CK Wagaba <ckwagaba@gmail.com>
2018-02-24 15:27:53 +0300

    make predictions using SVM and calculate effeciency

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 
-""" 
+"""
     This is the code to accompany the Lesson 2 (SVM) mini-project.
 
-    Use a SVM to identify emails from the Enron corpus by their authors:    
+    Use a SVM to identify emails from the Enron corpus by their authors:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -19,12 +19,22 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+# import SVC
+from sklearn.svm import SVC
 
+# create classifier
+clf = SVC(kernel='linear')
 
+# train the classifier
+clf.fit(features_train, labels_train)
 
-#########################################################
-### your code goes here ###
+# make predictions
+pred = clf.predict(features_test)
+print pred
 
-#########################################################
+# import accuracy score
+from sklearn.metrics import accuracy_score
 
-
+# calculate effeciency
+score = accuracy_score(labels_test, pred)
+print score
