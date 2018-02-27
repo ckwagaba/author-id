ac581b821e4b1055e2bc95ea882ded86fea292af
CK Wagaba <ckwagaba@gmail.com>
2018-02-28 00:24:52 +0300

    print prediction and accuracy

--- choose_your_own/knn.py
+++ choose_your_own/knn.py
@@ -20,14 +20,14 @@ grade_slow = [features_train[ii][0] for ii in range(0, len(features_train)) if l
 bumpy_slow = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==1]
 
 #### initial visualization
-plt.xlim(0.0, 1.0)
-plt.ylim(0.0, 1.0)
-plt.scatter(bumpy_fast, grade_fast, color = "b", label="fast")
-plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
-plt.legend()
-plt.xlabel("bumpiness")
-plt.ylabel("grade")
-plt.show()
+# plt.xlim(0.0, 1.0)
+# plt.ylim(0.0, 1.0)
+# plt.scatter(bumpy_fast, grade_fast, color = "b", label="fast")
+# plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
+# plt.legend()
+# plt.xlabel("bumpiness")
+# plt.ylabel("grade")
+# plt.show()
 
 from sklearn import neighbors
 from sklearn.metrics import accuracy_score
@@ -38,10 +38,10 @@ clf.fit(features_train, labels_train)
 pred = clf.predict(features_test)
 print pred
 
-acc = accuracy_score(labels_train, pred)
+acc = accuracy_score(labels_test, pred)
 print acc
 
-try:
-    prettyPicture(clf, features_test, labels_test)
-except NameError:
-    pass
+# try:
+#     prettyPicture(clf, features_test, labels_test)
+# except NameError:
+#     pass
