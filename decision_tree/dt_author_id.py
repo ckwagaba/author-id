ddcc88cf897ab685ae325e09ccea9c411bb3ad31
CK Wagaba <ckwagaba@gmail.com>
2018-02-27 10:35:07 +0300

    tune using min_samples_split

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -22,7 +22,7 @@ features_train, features_test, labels_train, labels_test = preprocess()
 from sklearn import tree
 from sklearn.metrics import accuracy_score
 
-clf = tree.DecisionTreeClassifier()
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
 clf.fit(features_train, labels_train)
 pred = clf.predict(features_test)
 
