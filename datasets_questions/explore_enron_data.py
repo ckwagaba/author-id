1f182acf99808a042a5ef1ffce746114e822d339
CK Wagaba <ckwagaba@gmail.com>
2018-03-13 19:39:25 +0300

    get number of folks with quantified salaries

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -19,10 +19,10 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
-have_email_address = 0
+have_quantified_salary = 0
 
 for p in enron_data:
-    if enron_data[p]["email_address"] != "NaN":
-        have_email_address += 1
+    if enron_data[p]["salary"] != "NaN":
+        have_quantified_salary += 1
 
-print have_email_address
+print have_quantified_salary
