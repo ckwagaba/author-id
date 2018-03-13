6f79dea5eba84b319f6ae3f59fc35f94a20a5c8f
CK Wagaba <ckwagaba@gmail.com>
2018-03-13 19:04:29 +0300

    get exercised stock options

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -19,4 +19,4 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
-print enron_data["COLWELL WESLEY"]["from_this_person_to_poi"]
+print enron_data["SKILLING JEFFREY K"]["exercised_stock_options"]
